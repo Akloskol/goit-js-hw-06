a237be971be28c1481ec6bea1f49436ae81b5c5d
Duseldof777 <va.madunov@gmail.com>
2023-07-31 22:31:23 +0300

    1

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const markup = ingredients.map((el) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = el;
+  return li;
+});
+list.append(...markup);
+
+console.log(markup);
+console.log(list);
---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+const value = {
+  inputId: document.querySelector("#validation-input"),
+};
+
+value.inputId.addEventListener("blur", handlerSearch);
+
+// console.dir(value.inputId);
+
+function handlerSearch(evt) {
+  if (
+    evt.currentTarget.value.length === Number(evt.currentTarget.dataset.length)
+  ) {
+    evt.currentTarget.classList.remove("invalid");
+    evt.currentTarget.classList.add("valid");
+  } else {
+    evt.currentTarget.classList.add("invalid");
+  }
+  console.log(evt.currentTarget.value.length);
+}
